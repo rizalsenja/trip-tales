c3654c8ff81af823c5c6b35f7478346206e51d4d
Rizal <rizal@senja.co.uk>
2025-07-02 15:36:09 +0700

    created button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from '../assets/styles/components/Button.module.scss';
+
+interface ButtonProps {
+	label: string;
+	variant?: 'filled' | 'outline' | 'text' | 'link';
+	size?: 'xl' | 'md' | 'sm';
+	color?: 'primary' | 'secondary' | 'red';
+	disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+	label,
+	variant = 'filled',
+	size = 'md',
+	color = 'primary',
+	disabled = false,
+}) => {
+	return (
+		<button className={`${styles[variant]} ${styles[size]} ${styles[color]}`} disabled={disabled}>
+			{label}
+		</button>
+	);
+};
+
+export default Button;
