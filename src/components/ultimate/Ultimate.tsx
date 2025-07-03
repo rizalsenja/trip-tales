27160f5c7768bd9b2051e037e10c40a3a40d95a4
Yabes <yabes@senja.co.uk>
2025-07-03 17:36:32 +0700

    fix padding ultimate

--- src/components/ultimate/Ultimate.tsx
+++ src/components/ultimate/Ultimate.tsx
@@ -8,47 +8,51 @@ import item4 from "../../assets/ultimate/item-4.webp";
 import Button from "../ui/Button.tsx/Button";
 
 const features = [
-  {
-    icon: item1,
-    title: "Trip Planner",
-    desc: "Plan trips with flights, hotels, activities & collaborators",
-  },
-  {
-    icon: item2,
-    title: "Destination Discovery",
-    desc: "Explore destinations by season, budget, time & vibe",
-  },
-  {
-    icon: item3,
-    title: "Social Sharing",
-    desc: "Post your trip stories & see others’ real experiences",
-  },
-  {
-    icon: item4,
-    title: "Save & Reuse Plans",
-    desc: "Post your trip stories & see others’ real experiences",
-  },
+    {
+        icon: item1,
+        title: "Trip Planner",
+        desc: "Plan trips with flights, hotels, activities & collaborators",
+    },
+    {
+        icon: item2,
+        title: "Destination Discovery",
+        desc: "Explore destinations by season, budget, time & vibe",
+    },
+    {
+        icon: item3,
+        title: "Social Sharing",
+        desc: "Post your trip stories & see others’ real experiences",
+    },
+    {
+        icon: item4,
+        title: "Save & Reuse Plans",
+        desc: "Post your trip stories & see others’ real experiences",
+    },
 ];
 
 export default function Ultimate() {
-  return (
-    <section className={styles.ultimate}>
-       <Button label='Features' size='xl' variant='outline' color='primary' />      
-       <h2 className={styles.ultimate__title}>Your Ultimate Travel Companion</h2>
-      <div className={styles.ultimate__list}>
-        {features.map((feature, idx) => (
-          <div className={styles.ultimate__card} key={idx}>
-            <img
-              src={feature.icon}
-              alt={feature.title}
-              className={styles.ultimate__icon}
-              loading="lazy"
-            />
-            <h3 className={styles.ultimate__cardTitle}>{feature.title}</h3>
-            <p className={styles.ultimate__cardDesc}>{feature.desc}</p>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.ultimate}>
+            <Button label='Features' size='xl' variant='outline' color='primary' />
+            <h2 className={styles.ultimate__title}>Your Ultimate Travel Companion</h2>
+            <div className={styles.ultimate__list}>
+                {features.map((feature, idx) => (
+                    <div className={styles.ultimate__card} key={idx}>
+                        <img
+                            src={feature.icon}
+                            alt={feature.title}
+                            className={styles.ultimate__icon}
+                            loading="lazy"
+                        />
+                        <div className={styles.ultimate__cardContent}>
+                            <h3 className={styles.ultimate__cardTitle}>{feature.title}</h3>
+                            <p className={styles.ultimate__cardDesc}>{feature.desc}</p>
+
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 }
