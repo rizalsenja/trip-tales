48e3792025aa907572df04ffe74150434e42b5a5
Rizal <rizal@senja.co.uk>
2025-07-03 12:09:29 +0700

    hide navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 // import './App.css'
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </React.Fragment>
   )
 }
