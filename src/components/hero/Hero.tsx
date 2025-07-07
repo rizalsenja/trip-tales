ea7e2bb4c76afb4963fba4bf9e0d998f8b51ca85
Rizal <rizal@senja.co.uk>
2025-07-07 13:48:22 +0700

    used firebase

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,8 +1,45 @@
+import { useState } from 'react';
 import styles from './Hero.module.scss'
 import heroImg from '../../assets/img/hero-img2.webp'
 import Button from '../ui/Button.tsx/Button';
+import { db } from '../../firebase.config';
+import { collection, addDoc } from 'firebase/firestore';
 
 const Hero = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        country: '',
+        email: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!formData.name.trim() || !formData.country.trim() || !formData.email.trim()) {
+            console.log('Please input all field.');
+            return;
+        }
+
+        try {
+            await addDoc(collection(db, 'signups'), {
+            ...formData,
+            createdAt: new Date(),
+        });
+
+            console.log('Thank you for signing up!');
+            setFormData({ name: '', country: '', email: '' });
+        } catch (error) {
+            console.error('Error saving to Firestore:', error);
+        }
+    };
+
 	return (
 		<section className={styles.hero}>
             <div className={styles.hero__bg__img__container}>
@@ -32,21 +69,21 @@ const Hero = () => {
 					Be Among the First to Explore TripTales
 				</h2>
 
-				<form className={styles.hero__form__input}>
+				<form className={styles.hero__form__input} onSubmit={handleSubmit}>
                     <div className={styles.hero__form__input__cta}>
                         <div className={styles.hero__field}>
                             <span>Name</span>
-                            <input type='text' placeholder='Enter name' />
+                            <input type='text' placeholder='Enter name' name='name' value={formData.name} onChange={handleChange} />
                         </div>
 
                         <div className={styles.hero__field}>
                             <span>Country</span>
-                            <input type='text' placeholder='Search country' />
+                            <input type='text' placeholder='Search country' name='country' value={formData.country} onChange={handleChange} />
                         </div>
 
                         <div className={styles.hero__field}>
                             <span>Email Address</span>
-                            <input type='email' placeholder='Enter email address' />
+                            <input type='email' placeholder='Enter email address' name='email' value={formData.email} onChange={handleChange} />
                         </div>
                     </div>
 
