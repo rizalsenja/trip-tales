ebe0ffa2e1ac788d73d745dfc41c0574c820d9fd
Rizal <rizal@senja.co.uk>
2025-07-08 17:30:49 +0700

    added admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home/Home';
 import Footer from './components/footer/Footer';
+import Admin from './pages/Admin/Admin';
 // import './App.css'
 
 function App() {
+  const { pathname } = useLocation();
+  const isAdmin = pathname.startsWith('/admin');
+
   return (
     <React.Fragment>
-      <Navbar />
+      {/* <Navbar /> */}
+      {!isAdmin && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
+      {!isAdmin && <Footer />}
     </React.Fragment>
   )
 }
