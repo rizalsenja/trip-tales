e2337c22e4f47ad5af7a6bd24859efad87f1cfe0
Rizal <rizal@senja.co.uk>
2025-07-09 15:14:16 +0700

    added require auth

---
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../hook/useAuth';
+import type { JSX } from 'react';
+import Loader from '../ui/Loader/Loader';
+
+const RequireAuth = ({ children }: { children: JSX.Element }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) return <Loader />;
+  if (!user)  return <Navigate to="/login" replace />;
+  return children;
+};
+
+export default RequireAuth
