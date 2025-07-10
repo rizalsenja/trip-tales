784ead5a4758ff0e71097c490f8f6a9149df7cd7
Rizal <rizal@senja.co.uk>
2025-07-10 13:59:29 +0700

    change footer location

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/ui/navbar/Navbar';
 import Home from './pages/Home/Home';
-import Footer from './components/footer/Footer';
+import Footer from './components/ui/footer/Footer';
 import Admin from './pages/Admin/Admin';
 import Login from './pages/LoginAdmin'
 import ToastProvider from './components/ui/Toast/ToastProvider';
